ba9f1c2d43b93c1fd7e8470daf1c0534ef5843f0
Yahav Tzukerman <tzuki.yahav@gmail.com>
2022-07-05 18:01:30 +0300

    Fixing Bugs on Pizza Resource

--- src/main/java/com/starshaf/pizzaup/converters/PizzaEntityToPizzaModelConverter.java
+++ src/main/java/com/starshaf/pizzaup/converters/PizzaEntityToPizzaModelConverter.java
@@ -3,7 +3,11 @@ package com.starshaf.pizzaup.converters;
 import com.starshaf.pizzaup.entities.PizzaEntity;
 import com.starshaf.pizzaup.models.PizzaModel;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+import javax.persistence.Column;
+
+@Component
 public class PizzaEntityToPizzaModelConverter implements Converter<PizzaEntity, PizzaModel> {
     @Override
     public PizzaModel convert(PizzaEntity pizzaEntity) {
--- src/main/java/com/starshaf/pizzaup/models/PizzaModel.java
+++ src/main/java/com/starshaf/pizzaup/models/PizzaModel.java
@@ -2,10 +2,14 @@ package com.starshaf.pizzaup.models;
 
 import com.starshaf.pizzaup.enums.Size;
 import com.starshaf.pizzaup.enums.Toppings;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class PizzaModel {
     private int id;
