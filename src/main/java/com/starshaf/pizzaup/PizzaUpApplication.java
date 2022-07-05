69da3a08a888960991a3457cccc7432fa6846499
sagif <sagif1z3@gmail.com>
2022-07-05 16:37:48 +0300

    First commit

---
+++ src/main/java/com/starshaf/pizzaup/PizzaUpApplication.java
@@ -0,0 +1,12 @@
+package com.starshaf.pizzaup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzaUpApplication.class, args);
+    }
+}
---
+++ src/test/java/com/starshaf/pizzaup/PizzaUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starshaf.pizzaup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
